b674917a191b52cfc8629f7c533fb5be429ba813
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2023-01-16 12:24:35 +0530

    Code update

--- src/components/SnackPlayGround.jsx
+++ src/components/SnackPlayGround.jsx
@@ -1,30 +1,17 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import FoodSound from "../assets/music/food.mp3" 
 import GameOverSound from "../assets/music/gameover.mp3" 
 import MoveSound from "../assets/music/move.mp3" 
 
-const SnackPlayGround = () => {
-  const foodSound = new Audio(FoodSound)
-  const gameOverSound = new Audio(GameOverSound)
-  const moveSound = new Audio(MoveSound)
-  const board = useRef()
-  let lastTime = 0
-  let snack = [{x: 4, y: 4}]
-  let valocity = {x: 0, y: 0}
-  let foodXY = {x: 10, y: 10}
+export default function SnackPlayGround() {
+  const board = useRef(null)
+  const frame = useRef(0)
+  const food = useRef({x: 10, y: 10})
+  const [lastTime, setLastTime] = useState(0)
+  const [snack, setSnack] = useState([{x: 4, y: 4}])
+  const [valocity, setValocity] = useState({x: 0, y: 0})
 
-  function main(currentTime){
-    window.requestAnimationFrame(main);
-    
-    if((currentTime - lastTime) / 1000 < 1 / 5){
-      return
-    }
-
-    lastTime = currentTime
-    gameEngine()
-  }
-
-  function isCollide(){
+  const isCollide = useCallback(() => {
     if(snack[0].x >= 16 || snack[0].x <= 0 || snack[0].y >= 16 || snack[0].y <= 0){
       return true
     }
@@ -36,22 +23,27 @@ const SnackPlayGround = () => {
     }
 
     return false
-  } 
-  
-  function gameEngine(){
+  }, [snack])
+
+  const gameEngine = useCallback(() => {
+    const foodSound = new Audio(FoodSound)
+    const gameOverSound = new Audio(GameOverSound)
     board.current.innerHTML = ''
 
     if(isCollide()){
       gameOverSound.play()
-      snack = [{x: 4, y: 4}]
-      valocity = {x: 0, y: 0}
-    }
-
-    if(snack[0].x === foodXY.x && snack[0].y === foodXY.y){
+      setSnack([{x: 4, y: 4}])
+      setValocity({x: 0, y: 0})
+    } 
+    
+    if(snack[0].x === food.current.x && snack[0].y === food.current.y){
       foodSound.play()
-      snack.unshift({x: snack[0].x + valocity.x, y: snack[0].y + valocity.y})
-      foodXY = {x: 1 + Math.round(1 + (14 - 1) * Math.random()), y: 1 + Math.round(1 + (14 - 1) * Math.random())}
-    }
+      
+      snack.push({x: snack[0].x + valocity.x, y: snack[0].y + valocity.y})
+
+      food.current.x = 1 + Math.round(1 + (14 - 1) * Math.random())
+      food.current.y = 1 + Math.round(1 + (14 - 1) * Math.random())
+    }    
 
     for(let i = snack.length - 2; i >= 0; i--){
       snack[i + 1] = {...snack[i]}
@@ -59,52 +51,67 @@ const SnackPlayGround = () => {
 
     snack[0].x += valocity.x
     snack[0].y += valocity.y
- 
+    
     snack.forEach((e, index) => {
-      let snack = document.createElement('div')
-      snack.style.gridRowStart = e.y
-      snack.style.gridColumnStart = e.x
+      let newSnack = document.createElement('div')
+      newSnack.style.gridRowStart = e.y
+      newSnack.style.gridColumnStart = e.x
       
       if(index){
-        snack.classList.add('snack') 
+        newSnack.classList.add('snack') 
       } else {
-        snack.classList.add('head')
+        newSnack.classList.add('head')
       }
       
-      board.current?.appendChild(snack)
+      board.current.appendChild(newSnack)
     })   
     
-    let food = document.createElement('div')
-    food.style.gridRowStart = foodXY.y
-    food.style.gridColumnStart = foodXY.x
-    food.classList.add('food')
-    board.current?.appendChild(food)
-  }
+    let newFood = document.createElement('div')
+    newFood.style.gridRowStart = food.current.y
+    newFood.style.gridColumnStart = food.current.x
+    newFood.classList.add('food')
+    board.current.appendChild(newFood)
+  }, [snack, valocity, food, isCollide])
 
-  window.addEventListener('keydown', (e) => {
-    moveSound.play()
+  useEffect(() => {
+    window.addEventListener('keydown', (e) => {
+      const moveSound = new Audio(MoveSound)
+      moveSound.play()
 
-    switch (e.key) { 
-      case 'ArrowUp':
-        valocity = {x: 0, y: -1}
-        break;
-      case 'ArrowDown':
-        valocity = {x: 0, y: 1}
-        break;
-      case 'ArrowLeft':
-        valocity = {x: -1, y: 0}
-        break;
-      case 'ArrowRight':
-        valocity = {x: 1, y: 0}
-        break;
-      default:
-        break;
-    }    
-  }) 
+      switch (e.key) { 
+        case 'ArrowUp':
+          setValocity({x: 0, y: -1})
+          break;
+        case 'ArrowDown':
+          setValocity({x: 0, y: 1})
+          break;
+        case 'ArrowLeft':
+          setValocity({x: -1, y: 0})
+          break;
+        case 'ArrowRight':
+          setValocity({x: 1, y: 0})
+          break;
+        default:
+          break;
+      }
+    })
+  }, [])
 
-  useEffect(() => {  
-    window.requestAnimationFrame(main)
-  })
+  const animate = useCallback((currentTime) => {
+    frame.current = requestAnimationFrame(animate)
+    
+    if((currentTime - lastTime) / 1000 < 1 / 5){
+      return
+    }
+
+    setLastTime(currentTime)
+    gameEngine()
+  }, [lastTime, gameEngine])
+
+  useEffect(() => {
+    frame.current = requestAnimationFrame(animate)
+    return () => cancelAnimationFrame(frame.current)
+  }, [animate])
  
   return (
     <div className='body'>
@@ -112,5 +119,3 @@ const SnackPlayGround = () => {
     </div>
   )
 }
-
-export default SnackPlayGround
